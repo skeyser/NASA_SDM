97754f861ae1dac74b7cfd3f79316fe1997ede62
Spencer Keyser <skeyser@wisc.edu>
2023-01-16 15:42:15 -0600

    Continued work on rgbif script with expert range map

--- Mammal_Code/Mammal_GBIF_Maxent.R
+++ Mammal_Code/Mammal_GBIF_Maxent.R
@@ -10,7 +10,7 @@
 #fur bearer records and expert range maps.
 
 #Package install
-install.packages('rgbif')
+#install.packages('rgbif')
 
 #Set up rgbif account creds
 #usethis::edit_r_environ()
@@ -27,6 +27,10 @@ library(ggplot2)
 #Bobcat to start
 #This is the structure for remote data request via GBIF server
 name_backbone("Lynx rufus")$usageKey #find the identifier for the species
+name_backbone("Martes americana")$usageKey #find the identifier for the species
+name_backbone("Martes pennanti")$usageKey #find the identifier for the species
+name_backbone("Lontra canadensis")$usageKey #find the identifier for the species
+
 
 #Request the download
 bobcat_gbif <- occ_download(
@@ -42,25 +46,127 @@ bobcat_gbif <- occ_download(
   format = "SIMPLE_CSV" #Return a CSV
 )
 
+marten_gbif <- occ_download(
+  type = "and",
+  pred("taxonKey", 5218864), #species
+  pred("hasGeospatialIssue", FALSE), #remove geospatial issues
+  pred("hasCoordinate", TRUE), #Only take occurrence records with coordinates
+  pred("occurrenceStatus", "PRESENT"), #Only take where confirmed presence
+  pred_gte("year", "1900"), #Only for years greater than 1900
+  pred_not(pred_in("basisOfRecord", c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))), #Remove fossils and living specimens
+  pred("country", "US"), #Take only for the US
+  pred_or(pred_lt("coordinateUncertaintyInMeters",10000),pred_isnull("coordinateUncertaintyInMeters")), #Keep numbers only for points with at least 10km spatial accuracy
+  format = "SIMPLE_CSV" #Return a CSV
+)
+
+fisher_gbif <- occ_download(
+  type = "and",
+  pred("taxonKey", 5218855), #species
+  pred("hasGeospatialIssue", FALSE), #remove geospatial issues
+  pred("hasCoordinate", TRUE), #Only take occurrence records with coordinates
+  pred("occurrenceStatus", "PRESENT"), #Only take where confirmed presence
+  pred_gte("year", "1900"), #Only for years greater than 1900
+  pred_not(pred_in("basisOfRecord", c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))), #Remove fossils and living specimens
+  pred("country", "US"), #Take only for the US
+  pred_or(pred_lt("coordinateUncertaintyInMeters",10000),pred_isnull("coordinateUncertaintyInMeters")), #Keep numbers only for points with at least 10km spatial accuracy
+  format = "SIMPLE_CSV" #Return a CSV
+)
+
+otter_gbif <- occ_download(
+  type = "and",
+  pred("taxonKey", 2433727), #species
+  pred("hasGeospatialIssue", FALSE), #remove geospatial issues
+  pred("hasCoordinate", TRUE), #Only take occurrence records with coordinates
+  pred("occurrenceStatus", "PRESENT"), #Only take where confirmed presence
+  pred_gte("year", "1900"), #Only for years greater than 1900
+  pred_not(pred_in("basisOfRecord", c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))), #Remove fossils and living specimens
+  pred("country", "US"), #Take only for the US
+  pred_or(pred_lt("coordinateUncertaintyInMeters",10000),pred_isnull("coordinateUncertaintyInMeters")), #Keep numbers only for points with at least 10km spatial accuracy
+  format = "SIMPLE_CSV" #Return a CSV
+)
+
+
 #check the order wait time
-occ_download_wait(bobcat_gbif)
+occ_download_wait(marten_gbif)
 
 #Load in the data
-d <- occ_download_get(bobcat_gbif) %>%
+bobcat <- occ_download_get(bobcat_gbif) %>%
   occ_download_import()
 
-names(d)
+marten <- occ_download_get(marten_gbif) %>%
+  occ_download_import()
+
+fisher <- occ_download_get(fisher_gbif) %>%
+  occ_download_import()
+
+otter <- occ_download_get(otter_gbif) %>%
+  occ_download_import()
+
+
+#Once the file has been downloaded we can read it into R via the file key
+#saved locally in the project.
+bobcat <- occ_download_import(key = "0242490-220831081235567")
+marten <- occ_download_import(key = "0249247-220831081235567")
+fisher <- occ_download_import(key = "0249196-220831081235567")
+otter <- occ_download_import(key = "0249198-220831081235567")
+
+names(bobcat)
+
+#fur
+fur <- rbind(bobcat, marten, fisher, otter)
+
 
 #Make bobcat a spatial DF
-d.sf <- d %>%
+fur.sf <- fur %>%
   rename("Lat" = "decimalLatitude") %>%
   rename("Long" = "decimalLongitude") %>%
   filter(!is.na(coordinateUncertaintyInMeters) & coordinateUncertaintyInMeters <= 3000) %>%
   filter(year >= 2003) %>%
   st_as_sf(coords = c("Long", "Lat"), crs = 4326) 
 
-#Visualize the occurrence points
-ggplot(d.sf) +
-  geom_sf()
+table(fur.sf$species)
 
-#Next we need to load in the environmental data
+
+#Visualize the occurrence points
+ggplot(fur.sf) +
+  geom_sf(aes(color = species))
+
+#Next we need to bring in the expert range maps
+mammal.erm <- st_read("F:/IUCN/MAMMS_ALLSPP.shp")
+str(mammal.erm)
+
+#Select a species of interest
+bobcat.erm <- mammal.erm %>% filter(Species == "Lynx rufus")
+marten.erm <- mammal.erm %>% filter(Species == "Martes americana")
+fisher.erm <- mammal.erm %>% filter(Species == "Martes pennanti")
+otter.erm <- st_read("F:/IUCN/IUCN_MissingSpp/Lontra_canadensis/data_0.shp")
+
+#Visualize the expert range map
+fur.sf %>%
+  filter(species == "Lynx rufus") %>%
+  ggplot() +
+  geom_sf() + 
+  geom_sf(data = bobcat.erm, fill = NA, color = "red") +
+  theme_bw()
+  
+fur.sf %>%
+  filter(species == "Martes americana") %>%
+  ggplot() +
+  geom_sf() + 
+  geom_sf(data = marten.erm, fill = NA, color = "blue") + 
+  theme_bw()
+
+fur.sf %>%
+  filter(species == "Pekania pennanti") %>%
+  ggplot() +
+  geom_sf() + 
+  geom_sf(data = fisher.erm, fill = NA, color = "green") + 
+  theme_bw()
+
+fur.sf %>%
+  filter(species == "Lontra canadensis") %>%
+  ggplot() +
+  geom_sf() + 
+  geom_sf(data = otter.erm, fill = NA, color = "orange") + 
+  theme_bw()
+
